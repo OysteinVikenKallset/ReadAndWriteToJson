561bb3269e32bf63e388a8ec952da9d31e467023
Øystein Kallset <oysteinkallset@gmail.com>
2024-05-03 15:56:01 +0200

    Classer og Metoder

---
+++ ReadAndWriteToJson/Messages.cs
@@ -0,0 +1,10 @@
+using System;
+
+
+    public static class Messages
+    {
+        public static void greetings()
+        {
+            Console.WriteLine("Hello Sir, how are you today?");
+        }
+    }
